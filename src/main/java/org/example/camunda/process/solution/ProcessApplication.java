7c371dd43c42a4513712bf22aee67b45e5c8f33f
bszaj <b.szajor@gmail.com>
2023-02-08 16:30:59 +0100

    funkcje

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -4,10 +4,12 @@ import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
-import io.camunda.zeebe.spring.client.annotation.ZeebeVariable;
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.KeyStore;
 
 import java.util.UUID;
 import java.util.logging.ConsoleHandler;
@@ -22,12 +24,30 @@ import java.util.logging.Logger;
 public class ProcessApplication {
     static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     static ConsoleHandler handler = new ConsoleHandler();
+    private JobClient client;
+    private String recovery_certyficate_uuid;
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     SpringApplication.run(ProcessApplication.class, args);
-  }
+    logger.setLevel(Level.ALL);
+    handler.setLevel(Level.ALL);
+    logger.addHandler(handler);
 
-
   }
+@ZeebeWorker(type= "notify_person_to_quarantine", autoComplete = true)
+    public void notifyPersonToQuarantine(final JobClient client, final ActivatedJob job, @ZeebeWorker String person_uuid){
+      logger.info("Retrieving contact details for peroson" + person_uuid + "from external database ");
+      logger.info("Sent notification to person" + person_uuid + "to quarantine");
 }
+
+
+@ZeebeWorker(type="send_certyficate_of_recovery", autoComplete = true)
+public void sendCertyficateOfRecovery(final JobClient client, final ActivatedJob job, @ZeebeWorker String person_uuid, @ZeebeVariablesAsType String recovery_certyficate_uuid){
+    logger.info("Retrieving Recovery Certyficate "+ recovery_certyficate_uuid +"from external database" );
+    logger.info(" Retriving Recovery details for person "+ person_uuid + "from external database ");
+    logger.info("Sending Recovery Certficate to person "+ person_uuid + "Enjoy that ice-cream!");
+}
+
+  }
+
 
--- src/main/java/org/example/camunda/process/solution/service/MyService.java
+++ src/main/java/org/example/camunda/process/solution/service/MyService.java
@@ -9,3 +9,4 @@ public class MyService {
     return true;
   }
 }
+
