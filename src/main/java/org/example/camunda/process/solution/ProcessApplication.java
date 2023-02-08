3a007a3d6a3c1512088a33f6d141a0a2acc82b25
bszaj <b.szajor@gmail.com>
2023-02-08 16:36:49 +0100

    funkcje

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -4,14 +4,12 @@ import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariable;
 import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.security.KeyStore;
-
-import java.util.UUID;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,14 +33,14 @@ public class ProcessApplication {
 
   }
 @ZeebeWorker(type= "notify_person_to_quarantine", autoComplete = true)
-    public void notifyPersonToQuarantine(final JobClient client, final ActivatedJob job, @ZeebeWorker String person_uuid){
+    public void notifyPersonToQuarantine(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid){
       logger.info("Retrieving contact details for peroson" + person_uuid + "from external database ");
       logger.info("Sent notification to person" + person_uuid + "to quarantine");
 }
 
 
 @ZeebeWorker(type="send_certyficate_of_recovery", autoComplete = true)
-public void sendCertyficateOfRecovery(final JobClient client, final ActivatedJob job, @ZeebeWorker String person_uuid, @ZeebeVariablesAsType String recovery_certyficate_uuid){
+public void sendCertyficateOfRecovery(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid, @ZeebeVariablesAsType String recovery_certyficate_uuid){
     logger.info("Retrieving Recovery Certyficate "+ recovery_certyficate_uuid +"from external database" );
     logger.info(" Retriving Recovery details for person "+ person_uuid + "from external database ");
     logger.info("Sending Recovery Certficate to person "+ person_uuid + "Enjoy that ice-cream!");
