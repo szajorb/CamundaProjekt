fe6c8b23812bea61c8bbe1267f6304b48a2b3771
Bertil02 <dominik27p@gmail.com>
2023-02-08 06:30:25 +0100

    initial

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -1,16 +1,33 @@
 package org.example.camunda.process.solution;
 
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariable;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 @SpringBootApplication
 @EnableZeebeClient
 @Deployment(resources = "classpath*:/models/*.*")
 public class ProcessApplication {
+    static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    static ConsoleHandler handler = new ConsoleHandler();
 
   public static void main(String[] args) {
     SpringApplication.run(ProcessApplication.class, args);
   }
+
+
+  }
 }
+
--- src/main/java/org/example/camunda/process/solution/ProcessConstants.java
+++ src/main/java/org/example/camunda/process/solution/ProcessConstants.java
@@ -2,5 +2,5 @@ package org.example.camunda.process.solution;
 
 public class ProcessConstants {
 
-  public static final String BPMN_PROCESS_ID = "camunda-process";
+  public static final String BPMN_PROCESS_ID = "booksbpmn";
 }
