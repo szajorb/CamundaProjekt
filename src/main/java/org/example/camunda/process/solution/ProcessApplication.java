6307224783780c40a1b9a722fb35c89fed6a08a2
bszaj <b.szajor@gmail.com>
2023-02-08 19:27:56 +0100

    funkcje

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -4,17 +4,12 @@ import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
-import io.camunda.zeebe.spring.client.annotation.ZeebeVariable;
-import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
 
 @SpringBootApplication
 @EnableZeebeClient
@@ -22,30 +17,26 @@ import java.util.logging.Logger;
 public class ProcessApplication {
     static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     static ConsoleHandler handler = new ConsoleHandler();
-    private JobClient client;
-    private String recovery_certyficate_uuid;
 
     public static void main(String[] args) {
-    SpringApplication.run(ProcessApplication.class, args);
-    logger.setLevel(Level.ALL);
-    handler.setLevel(Level.ALL);
-    logger.addHandler(handler);
+        SpringApplication.run(ProcessApplication.class, args);
+        logger.setLevel(Level.ALL);
+        handler.setLevel(Level.ALL);
+        logger.addHandler(handler);
+    }
 
-  }
-@ZeebeWorker(type= "notify_person_to_quarantine", autoComplete = true)
-    public void notifyPersonToQuarantine(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid){
-      logger.info("Retrieving contact details for peroson" + person_uuid + "from external database ");
-      logger.info("Sent notification to person" + person_uuid + "to quarantine");
+    @ZeebeWorker(type = "mail", autoComplete = true)
+    public void sendMail(final JobClient client, final ActivatedJob job) {
+        logger.info("Wyślij upomnienie o oddaniu ksiażki.");
+    }
+
+    @ZeebeWorker(type = "naloz_kare", autoComplete = true)
+    public void givePenalty(final JobClient client, final ActivatedJob job) {
+        logger.info("Nałóż karę za przetrzymywanie książki po terminie.");
+    }
+
+    @ZeebeWorker(type = "info", autoComplete = true)
+    public void sendInfo(final JobClient client, final ActivatedJob job) {
+        logger.info("Wyślij informację o nałożeniu kary.");
+    }
 }
-
-
-@ZeebeWorker(type="send_certyficate_of_recovery", autoComplete = true)
-public void sendCertyficateOfRecovery(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid, @ZeebeVariablesAsType String recovery_certyficate_uuid){
-    logger.info("Retrieving Recovery Certyficate "+ recovery_certyficate_uuid +"from external database" );
-    logger.info(" Retriving Recovery details for person "+ person_uuid + "from external database ");
-    logger.info("Sending Recovery Certficate to person "+ person_uuid + "Enjoy that ice-cream!");
-}
-
-  }
-
-
